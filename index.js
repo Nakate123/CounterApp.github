de11a33edf98ebf97af8d868b6f7f4744fc01ce3
Nakate123 <94731837+Nakate123@users.noreply.github.com>
2022-03-16 17:46:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Con from './Con';
+
+ReactDOM.render(
+  <>
+  {/* <React.StrictMode>
+    <App />
+  </React.StrictMode> */}
+  <Con />
+  </>,
+  document.getElementById('root')
+);
+
+
